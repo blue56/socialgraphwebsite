f9d02fdbc867bc2d6a985961d76036b5ff90c2a6
Jacob Kofod <jacob_kofod@yahoo.com>
2021-11-24 09:31:33 +0100

    Added some basic stuff

---
+++ streamlit_app.py
@@ -0,0 +1,11 @@
+from collections import namedtuple
+import altair as alt
+import math
+import pandas as pd
+import streamlit as st
+
+"""
+# Welcome to the project website for the course Social graphs and interactions (02805)
+# Made by Mihaela-Elena Nistor, Viktor Anzhelev Tsanev and Jacob Kofod
+
+"""
