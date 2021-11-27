784782a6af5de43d2d7ff1aad947c772a39e874d
Jacob Kofod <jacob_kofod@yahoo.com>
2021-11-27 14:33:18 +0100

    Added extra code

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,41 @@ import math
 import pandas as pd
 import streamlit as st
 
+# ===
+# Import packages: 
+import numpy as np 
+import pandas as pd 
+import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+import nltk
+from os import listdir
+from os.path import isfile, join
+from wordcloud import WordCloud
+from operator import itemgetter
+import random
+import community as com
+import os
+from fa2 import ForceAtlas2
+from matplotlib.pyplot import figure
+import powerlaw
+import networkx.algorithms.community as nx_comm
+import community as community_louvain
+import json
+import collections
+from nltk.tokenize import sent_tokenize,word_tokenize, RegexpTokenizer 
+from labMTsimple.storyLab import emotionFileReader, emotion, stopper, emotionV
+from nltk.stem import WordNetLemmatizer 
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+import re
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import itertools
+import urllib
+import imdb
+# ===
+
 """
 # Welcome to the project website for the course Social graphs and interactions (02805)
 # Made by Mihaela-Elena Nistor, Viktor Anzhelev Tsanev and Jacob Kofod
@@ -20,3 +55,32 @@ df_characters = pd.read_csv(characters_path)
 print(df_characters)
 
 data_load_state.text('Loading data...done!')
+
+# ====
+# Create undirected graph
+G = nx.Graph()
+
+# Read nodes from csv given as part of the exercises
+path_nodes = "nodes.csv"
+df_nodes = pd.read_csv(path_nodes)
+
+#print(df_nodes)
+
+# Add nodes to the graph
+for index, character in df_nodes.iterrows():
+    if (character["Name"]):
+        G.add_node(character["Name"])
+
+# Read edges from csv file. Edges where to and from referes to the same
+# node was been removed.
+path_edges = "edges.csv"
+df_edges = pd.read_csv(path_edges)
+
+# Load episodes
+path_episodes = "episodes.csv"
+df_episodes = pd.read_csv(path_episodes)
+
+#H.add_nodes_from(list_of_characters)
+friends_links = list(zip(df_edges.From, df_edges.To))
+G.add_edges_from(friends_links)
+# ====
